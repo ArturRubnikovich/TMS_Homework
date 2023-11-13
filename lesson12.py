73b825ae860dde8ffc2d9286827efaad080ca774
Artur Rubnikovich <arturredmu@gmail.com>
2023-11-13 11:26:42 +0300

    добавлены два задания

--- lesson12.py
+++ lesson12.py
@@ -1,27 +1,27 @@
-# 1
 from dataclasses import dataclass
+from abc import ABC, abstractmethod
 
 
+# 1
 @dataclass
 class Product:
     id: int
     name: str
     price: float
 
+
 # 2
 @dataclass
-class Pizza (Product):
+class Pizza(Product):
     topping: list
     spicy: str
     diameter: int
+
 
 @dataclass
 class Coffee(Product):
     cup_volume: float
     coffee_type: str
 
+
 # 3
-
-
-
-
