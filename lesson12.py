439bb7bbdde4d870bce7fe4998fca1487c10bcf7
Artur Rubnikovich <arturredmu@gmail.com>
2023-11-14 17:21:37 +0300

    добавлены 5 заданий

--- lesson12.py
+++ lesson12.py
@@ -14,7 +14,7 @@ class Product:
 @dataclass
 class Pizza(Product):
     topping: list
-    spicy: str
+    spicy: bool
     diameter: int
 
 
@@ -27,11 +27,11 @@ class Coffee(Product):
 # 3
 class AbstractShop(ABC):
     @abstractmethod
-    def add_product(self, key: Product):
+    def add_product(self, manufactured_product: Product):
         pass
 
     @abstractmethod
-    def sell_product(self, key: Product):
+    def sell_product(self, sold_product: Product):
         pass
 
     @abstractmethod
@@ -39,5 +39,37 @@ class AbstractShop(ABC):
         pass
 
 
+# 4-5
 class NonProductError(ValueError):
     pass
+
+
+class RealShop(AbstractShop):
+    def __init__(self):
+        self.real_shop = []
+
+    def add_product(self, manufactured_product: Product):
+        self.real_shop.append(manufactured_product)
+
+    def sell_product(self, sold_product: Product):
+        self.real_shop.remove(sold_product)
+
+    def all_products(self) -> list[Product]:
+        return self.real_shop
+
+    def is_valid1(self, manufactured_product: Product):
+        if not isinstance(manufactured_product, Product):
+            raise NonProductError(f"{manufactured_product} не является изготовленными пиццей либо кофе")
+        else:
+            return True
+
+    def is_valid2(self, sold_product: Product):
+        if not isinstance(sold_product, Product):
+            raise NonProductError(f"{sold_product} не является проданными пиццей либо кофе")
+        else:
+            return True
+
+
+# 6
+
+
